bfa20e6a59bd24b4b944496df35876a168c932e1
Doozy <dozdoozie@gmail.com>
2022-03-06 15:19:43 +0300

    main contract to connect presenter and view

---
+++ app/src/main/java/com/raywenderlich/wewatch/main/MainContract.kt
@@ -0,0 +1,10 @@
+package com.raywenderlich.wewatch.main
+
+class MainContract {
+    interface PresenterInterface {
+//TODO: add interface methods for Presenter
+    }
+    interface ViewInterface {
+//TODO: add interface methods for View
+    }
+}
--- app/src/main/java/com/raywenderlich/wewatch/main/MainPresenter.kt
+++ app/src/main/java/com/raywenderlich/wewatch/main/MainPresenter.kt
@@ -2,8 +2,7 @@ package com.raywenderlich.wewatch.main
 
 import com.raywenderlich.wewatch.model.LocalDataSource
 
-class MainPresenter(private var view: MainActivity, private var
-dataSource: LocalDataSource
+class MainPresenter(private var view: MainActivity, private var dataSource: LocalDataSource
 ) {
     private val TAG = "MainPresenter"
 }
