67d618f3bc6466e98fa2df0115689fdc8460d6bf
Doozy <dozdoozie@gmail.com>
2022-03-06 15:15:36 +0300

    main presenter kt file

---
+++ app/src/main/java/com/raywenderlich/wewatch/main/MainPresenter.kt
@@ -0,0 +1,9 @@
+package com.raywenderlich.wewatch.main
+
+import com.raywenderlich.wewatch.model.LocalDataSource
+
+class MainPresenter(private var view: MainActivity, private var
+dataSource: LocalDataSource
+) {
+    private val TAG = "MainPresenter"
+}
